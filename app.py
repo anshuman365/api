e22529b0b41fb7610b6632d64a809ea7a6c0c126
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-03-27 21:37:15 +0530

    Update in app and requrement

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app,async_mode='gevent')
 
 # Route for Monitor (Mobile A)
 @app.route("/monitor")
