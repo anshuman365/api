3b77052ea96fb719bd6c8d3ff2d54fb16d637846
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-04-02 20:51:54 +0530

    Fixed port binding & added accelerate

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def serve_image(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
